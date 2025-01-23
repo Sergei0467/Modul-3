c1adf6cf517fea4c85ae8636b0ac909021b068c5
Sergei <sledopit67@gmail.com>
2025-01-23 12:51:56 +0300

    Добавьте файлы проекта.

---
+++ Модуль 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace Modul__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            const string MyName = "Sergei";
+
+            Console.WriteLine(MyName);
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 38 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+
+
+            Console.ReadKey( );
+        }
+    }
+}
